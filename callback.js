c0b29aaa17ecc635589f86d5508185145406e2a9
Emily Dong <emilyhfdong@gmail.com>
2018-06-07 15:22:56 +0000

    print index when waldo is found

--- callback.js
+++ callback.js
@@ -1,18 +1,18 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  // for (var i = 0; i < arr.length; i++) {
-  //   if (arr[i] === "Waldo") {
-  //     found(i);   // execute callback
-  //   }
-  // }
-  var index = 0;
-  arr.forEach(function(element) {
-    if (element === 'Waldo') {
-      found(index);
-    } else {
-      index ++;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
     }
-  })
+  }
+  // var index = 0;
+  // arr.forEach(function(element) {
+  //   if (element === 'Waldo') {
+  //     found(index);
+  //   } else {
+  //     index ++;
+  //   }
+  // })
 }
 
 function actionWhenFound(index) {
